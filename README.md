f8543ee26f3f01a8df39d2cc1e154c86e27c7ef3
NealLyonsWake <waken.games@gmail.com>
2021-10-01 21:42:43 +0100

    Added the array, state and interaction functions to Greeting component. Updated README.md file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Greetings!
 This is my greetings app. This is an exercise for working through a Git workflow on code.
 The only thing to be added are new greetings in the greetingArray array which future iterations can demonstrate.
+
+The user clicks the button to generate a random greeting.
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,26 @@
 import React, {useEffect, useState}from "react";
 
 function Greeting(){
+    const [greeting, setGreeting] = useState([]);
+
+    const greetingArray =["Hello", "Hi there", "Alright", "Good morning", "How's it going", "How are ya", "Good Evening"]
+
+    useEffect(()=>{
+        newGreeting();
+      },[])
+      
+      function newGreeting(){ 
+          setGreeting(()=>{
+              return greetingArray[ Math.floor(Math.random()*greetingArray.length)]
+              })
+      }
+
+
     return (
-    <div><h1>Hello</h1></div>
+    <div>
+    <h1>{greeting}</h1>
+    <button onClick={newGreeting}>Click for new greeting</button>
+    </div>
     )
 }
 

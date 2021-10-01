afe169cc30398e1df432b12d6600b68736ea6949
NealLyonsWake <waken.games@gmail.com>
2021-10-01 22:22:28 +0100

    Added two new greetings to greetingArray

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -3,7 +3,7 @@ import React, {useEffect, useState}from "react";
 function Greeting(){
     const [greeting, setGreeting] = useState([]);
 
-    const greetingArray =["Hello", "Hi there", "Alright", "Good morning", "How's it going", "How are ya", "Good Evening", "Well 'ello mate", "Welcome", "Whatto"]
+    const greetingArray =["Hello", "Hi there", "Alright", "Good morning", "How's it going", "How are ya", "Good Evening", "Well 'ello mate", "Welcome", "Whatto", "Good Afternoon", "Hello Hello Hello"]
 
     useEffect(()=>{
         newGreeting();
